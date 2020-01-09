51bf3daead5e1761eff56c6fb384bf7b06d91d04
Viktor Soshko <Lorgar4@ukr.net>
2020-01-09 10:40:39 +0300

    add form handler and validation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
+import {isValid} from './utils';
 import './styles.css'
+
+
+const form = document.getElementById('form');
+const input = form.querySelector('#question-input');
+const submitBtn = form.querySelector('#submit');
+
+form.addEventListener('submit', submitFormHandler);
+input.addEventListener('input', (e) => {
+
+    submitBtn.disabled = ! isValid(e.target.value);
+    
+});
+
+function submitFormHandler(e) {
+    e.preventDefault();
+
+    if(isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+
+        submitBtn.disabled = true;
+        input.className = '';
+        input.value = '';
+
+        console.log(question);
+    }
+
+}
