7f72342983618ddfd9bfb9e2978525c5721fe936
Viktor Soshko <Lorgar4@ukr.net>
2020-01-09 09:01:58 +0300

    add style/css loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ module.exports = {
             template: './src/index.html'
         }),
         new CleanWebpackPlugin()
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 
 }
