0b90cbd9da4baa88498726c1267f00b805f579c4
Viktor Soshko <Lorgar4@ukr.net>
2020-01-09 12:24:32 +0300

    add list of questions

--- src/question.js
+++ src/question.js
@@ -9,8 +9,40 @@ export class Question {
         })
         .then( response => response.json())
         .then( response => {
-            console.log(response);
-            return response;
+            question.id = response.name;
+            return question;
         })
+        .then(addToLocalStorage)
+        .then(Question.renderList)
     }
+
+    static renderList() {
+        const questions = getQuestionsFromLocalStorage();  
+        const html = questions.length 
+            ? questions.map(toCard).join('')
+            : '<div class="mui--text-headline"> No questions yet </div>';
+        
+        const list = document.getElementById('list');
+
+        list.innerHTML = html;
+    }
+}
+
+function addToLocalStorage(question) {
+    const all = getQuestionsFromLocalStorage();
+    all.push(question);
+    localStorage.setItem('questions', JSON.stringify(all));
+}
+
+function getQuestionsFromLocalStorage() {
+    return JSON.parse(localStorage.getItem('questions') || '[]');
+}
+
+function toCard(question) {
+    return `<div class="mui--text-black-54">
+                ${new Date(question.date).toLocaleDateString()}
+                ${new Date(question.date).toLocaleTimeString()}
+            </div>
+            <br>
+            <div>${question.text}</div>`;
 }
